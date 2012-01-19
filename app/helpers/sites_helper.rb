db997d75f9daf2e44aefc968d146d4ed1f7505e2
andydenenberg <andydenenberg@gmail.com>
2012-01-18 22:38:29 -0600

    added css

--- app/helpers/sites_helper.rb
+++ app/helpers/sites_helper.rb
@@ -1,2 +1,12 @@
 module SitesHelper
+  
+  def server_state_color(status)
+    if status == 'SERVER_OK'
+       color = 'success' 
+    else
+       color = 'warning'
+    end
+    return color
+  end
+    
 end
