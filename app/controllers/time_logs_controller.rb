c17d0f79a1492c1ada21830fe6cbc823a59792a7
andydenenberg <andydenenberg@gmail.com>
2012-01-21 17:31:40 -0600

    time series from controller

---
+++ app/controllers/time_logs_controller.rb
@@ -0,0 +1,102 @@
+class TimeLogsController < ApplicationController
+
+  def main
+      @tl = Array.new
+      @t = Array.new
+      
+      iter = TimeLog.all
+      i = 0
+
+    iter.each do | tl |
+      @t = [tl.checked.strftime("%I:%M:%S%p"),tl.delay]
+      @tl.push @t
+      i=+ 1
+    end
+#    @tl = [["2012-01-21 11:00",0.003], ["2012-01-21 12:00",0.006], ["2012-01-21 13:00",0.001],["2012-01-21 14:00",0.001], ["2012-01-21 15:00",0.001],["2012-01-21 16:00",0.001],["2012-01-21 17:00",0.001],["2012-01-21 18:00",0.001], ["2012-01-21 19:00",0.001], ["2012-01-21 20:00",0.001]]
+  puts @tl
+  end
+  
+  # GET /time_logs
+  # GET /time_logs.json
+  def index
+    @time_logs = TimeLog.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @time_logs }
+    end
+  end
+
+  # GET /time_logs/1
+  # GET /time_logs/1.json
+  def show
+    @time_log = TimeLog.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @time_log }
+    end
+  end
+
+  # GET /time_logs/new
+  # GET /time_logs/new.json
+  def new
+    @time_log = TimeLog.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @time_log }
+    end
+  end
+
+  # GET /time_logs/1/edit
+  def edit
+    @time_log = TimeLog.find(params[:id])
+  end
+
+  # POST /time_logs
+  # POST /time_logs.json
+  def create
+    @time_log = TimeLog.new(params[:time_log])
+    
+    puts "Creating a new time_log"
+
+    respond_to do |format|
+      if @time_log.save
+        format.html { redirect_to @time_log, notice: 'Time log was successfully created.' }
+        format.json { render json: @time_log, status: :created, location: @time_log }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @time_log.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /time_logs/1
+  # PUT /time_logs/1.json
+  def update
+    @time_log = TimeLog.find(params[:id])
+
+    respond_to do |format|
+      if @time_log.update_attributes(params[:time_log])
+        format.html { redirect_to @time_log, notice: 'Time log was successfully updated.' }
+        format.json { head :ok }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @time_log.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /time_logs/1
+  # DELETE /time_logs/1.json
+  def destroy
+    @time_log = TimeLog.find(params[:id])
+    @time_log.destroy
+
+    respond_to do |format|
+      format.html { redirect_to time_logs_url }
+      format.json { head :ok }
+    end
+  end
+end
